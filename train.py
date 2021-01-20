398daac112f340df6f1a8cd268d3909f31321111
Ons CHERIF <77125529+ons-cherif@users.noreply.github.com>
2021-01-20 12:39:58 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ def main():
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 42,shuffle=True)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state = 42,shuffle=True)
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
